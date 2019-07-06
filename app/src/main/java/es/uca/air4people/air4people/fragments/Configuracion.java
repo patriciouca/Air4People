c55de7d9d4fa5968f59b6916ae9584fab009621f
patri <patricio.fernandezflorez@alum.uca.es>
2019-07-06 02:28:20 +0200

    Configuracion

--- app/src/main/java/es/uca/air4people/air4people/fragments/Configuracion.java
+++ app/src/main/java/es/uca/air4people/air4people/fragments/Configuracion.java
@@ -2,9 +2,16 @@ package es.uca.air4people.air4people.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import es.uca.air4people.air4people.R;
 
@@ -17,7 +24,8 @@ public class Configuracion extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment1, container, false);
+        final View view = inflater.inflate(R.layout.content_configuraciones, container, false);
+
+        return view;
     }
 }
